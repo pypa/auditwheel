a71dd34b653d92895b3cf918e87a695f0d9c4de4
Robert T. McGibbon <rmcgibbo@gmail.com>
2016-01-13 01:33:52 -0800

    Platform detection

--- deloc8/policy/__init__.py
+++ deloc8/policy/__init__.py
@@ -1,24 +1,45 @@
+import sys
 import json
-import platform
+import platform as _platform_module
 from typing import Optional
 from os.path import join, dirname, abspath
-_bitness, _linkage = platform.architecture()
+
+_sys_map = {'linux2': 'linux', 'linux': 'linux',
+            'darwin': 'osx', 'win32': 'win', 'openbsd5': 'openbsd'}
+non_x86_linux_machines = {'armv6l', 'armv7l', 'ppc64le'}
+platform = _sys_map.get(sys.platform, 'unknown')
+bits = 8 * tuple.__itemsize__
+linkage = _platform_module.architecture()[1]
+
+# XXX: this could be weakened. The show command _could_ run on OS X or
+# Windows probably, but there's not much reason to inspect foreign package
+# that won't run on the platform.
+if platform != 'linux':
+   print('Error: This tool only supports Linux', file=sys.stderr)
+   sys.exit(1)
+if linkage != 'ELF':
+   print(('Error: This tool only supports platforms that use the ELF '
+          'executable and linker format.'), file=sys.stderr)
+   sys.exit(1)
+
+
+if _platform_module.machine() in non_x86_linux_machines:
+    arch_name = machine()
+else:
+    arch_name = {64: 'x86_64', 32: '_i386'}[bits]
+
 
 with open(join(dirname(abspath(__file__)), 'policy.json')) as f:
     _POLICIES = json.load(f)
     for p in _POLICIES:
-        if _bitness == '32bit':
-            p['name'] = p['name'] + '_i386'
-        elif _bitness == '64bit':
-            p['name'] = p['name'] + '_x86_64'
+        p['name'] = p['name'] + '_' + arch_name
+
 
 POLICY_PRIORITY_HIGHEST = max(p['priority'] for p in _POLICIES)
 POLICY_PRIORITY_LOWEST = min(p['priority'] for p in _POLICIES)
 
 
 def load_policies():
-    if _linkage != 'ELF':
-        raise RuntimeError('Only ELF linkage is supported')
     return _POLICIES
 
 
