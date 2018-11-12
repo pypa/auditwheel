669d46355055a12713d7280718a5680da33fe117
Aaron Hall <aaronchallmba@gmail.com>
2018-11-12 15:48:22 -0500

    figure out pytest for Python 3.4

--- tests/travis.sh
+++ tests/travis.sh
@@ -5,6 +5,7 @@ set -exo pipefail
 if [[ "$WHEELHOUSE" == "1" ]]; then
     bash tests/test-wheelhouses.sh
 else
-    pytest -s --log-cli-level=25
+    # log-cli-level not currently supported by the pytest version used by the Python 3.4 build.
+    pytest -s --log-cli-level=25 || ( echo "$(pytest --version)" && pytest -s --log-level=25 )
     auditwheel lddtree $(python -c 'import sys; print(sys.executable)')
 fi
