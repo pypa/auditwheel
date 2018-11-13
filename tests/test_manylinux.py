f5af9701946f06b83351eb089237cf6416d67bb0
Aaron Hall, MBA <aaronchall@yahoo.com>
2018-11-12 21:02:08 -0500

    issue #109 Replace print statements with logger (#113)
    * Replace `print` statements with logger (issue #109)
    
    * Use pytest>=3.4
    
    * Configure pytest in setup.cfg

--- tests/test_manylinux.py
+++ tests/test_manylinux.py
@@ -6,8 +6,12 @@ import tempfile
 import shutil
 import warnings
 from distutils.spawn import find_executable
+import logging
 
-VERBOSE = True
+
+logger = logging.getLogger(__name__)
+
+
 ENCODING = 'utf-8'
 MANYLINUX_IMAGE_ID = 'quay.io/pypa/manylinux1_x86_64'
 DOCKER_CONTAINER_NAME = 'auditwheel-test-manylinux'
@@ -33,19 +37,16 @@ def docker_start(image, volumes={}, env_variables={}):
     """
     # Make sure to use the latest public version of the docker image
     cmd = ['docker', 'pull', image]
-    if VERBOSE:
-        print("$ " + " ".join(cmd))
+    logger.info("$ " + " ".join(cmd))
     output = check_output(cmd).decode(ENCODING).strip()
-    if VERBOSE:
-        print(output)
+    logger.info(output)
     cmd = ['docker', 'run', '-d']
     for guest_path, host_path in sorted(volumes.items()):
         cmd.extend(['-v', '%s:%s' % (host_path, guest_path)])
     for name, value in sorted(env_variables.items()):
         cmd.extend(['-e', '%s=%s' % (name, value)])
     cmd.extend([image, 'sleep', '10000'])
-    if VERBOSE:
-        print("$ " + " ".join(cmd))
+    logger.info("$ " + " ".join(cmd))
     return check_output(cmd).decode(ENCODING).strip()
 
 
@@ -54,11 +55,9 @@ def docker_exec(container_id, cmd):
     if isinstance(cmd, str):
         cmd = cmd.split()
     cmd = ['docker', 'exec', container_id] + cmd
-    if VERBOSE:
-        print("$ " + " ".join(cmd))
+    logger.info("$ " + " ".join(cmd))
     output = check_output(cmd).decode(ENCODING)
-    if VERBOSE:
-        print(output)
+    logger.info(output)
     return output
 
 
