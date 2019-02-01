359c5b3415ac13171dcfdebc3805a208fdb6ca59
Thomas Ferreira de Lima <tlima@princeton.edu>
2019-02-01 18:19:25 -0500

    pytest in travis is weirdly triggering a manual test

--- tests/pr134/hello_module/tests/manual_test.py
+++ tests/pr134/hello_module/tests/manual_test.py
@@ -1,2 +1,3 @@
-from hello import z_compress, z_uncompress
-assert z_uncompress(z_compress('test')) == 'test'
+if __name__ == '__main__':
+    from hello import z_compress, z_uncompress
+    assert z_uncompress(z_compress('test')) == 'test'
