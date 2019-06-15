654788516023029790944089355815afa5847033
Thomas Kluyver <thomas@kluyver.me.uk>
2019-06-15 18:27:17 +0100

    Switch wheelhouse script back to Nathaniel's wheels
    Robert's collection and Nikola's wheelhouse are both extinct

--- tests/integration/test-wheelhouses.sh
+++ tests/integration/test-wheelhouses.sh
@@ -1,19 +1,8 @@
 set -e
 
-# Download some wheels from one location
-PY_VERS="2.7 3.3 3.4 3.5"
-for PYVER in ${PY_VERS}; do
-    wget "https://github.com/getnikola/wheelhouse/archive/v${PYVER}.zip"
-    unzip "v${PYVER}.zip"
-done
-
 # Download some other wheels that Nathaniel made
-#URL=https://vorpus.org/~njs/tmp/manylinux-test-wheels/original/
-#curl --silent $URL | grep 'a href' | grep whl | cut -d ' ' -f 8 | cut -d '=' -f 2 | cut -d '"' -f 2 | xargs -n1 -I '{}' wget --no-check-certificate -P wheelhouse-njs "$URL/{}"
-
-# Download some more wheels that Robert made
-URL=http://stanford.edu/~rmcgibbo/wheelhouse/
-curl --silent $URL | grep 'a href' | grep whl | cut -d ' ' -f 8 | cut -d '=' -f 2 | cut -d '"' -f 2 | xargs -n1 -I '{}' wget -P wheelhouse-rmcgibbo "$URL/{}" -nc
+URL=https://vorpus.org/~njs/tmp/manylinux-test-wheels/original/
+curl --silent $URL | grep 'a href' | grep whl | cut -d ' ' -f 8 | cut -d '=' -f 2 | cut -d '"' -f 2 | xargs -n1 -I '{}' wget --no-check-certificate -P wheelhouse-njs "$URL/{}"
 
 # These are boring
 rm -f wheelhouse*/*-none-any.whl
