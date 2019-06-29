3cdef3e072e86f837e0f632f084212a5e37e6ccd
mayeut <mayeut@users.noreply.github.com>
2019-06-29 13:15:17 +0200

    Run linter on CI builds

--- tests/travis.sh
+++ tests/travis.sh
@@ -4,6 +4,8 @@ set -exo pipefail
 
 if [[ "$WHEELHOUSE" == "1" ]]; then
     bash tests/test-wheelhouses.sh
+elif [[ "$LINTER" == "1" ]]; then
+    tox -e lint
 else
     pytest -s
     auditwheel lddtree $(python -c 'import sys; print(sys.executable)')
