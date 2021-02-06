cf003a3d7825c8f655eebb8f9756d64cc4acf057
mayeut <mayeut@users.noreply.github.com>
2021-02-06 16:02:29 +0100

    Run Linter tests on GHA

--- tests/travis.sh
+++ tests/travis.sh
@@ -2,10 +2,6 @@
 
 set -exo pipefail
 
-if [[ "$LINTER" == "1" ]]; then
-    tox -e lint
-else
-    pytest -s --cov auditwheel --cov-branch
-    auditwheel lddtree $(python -c 'import sys; print(sys.executable)')
-    codecov || true  # Ignore failures from codecov tool
-fi
+pytest -s --cov auditwheel --cov-branch
+auditwheel lddtree $(python -c 'import sys; print(sys.executable)')
+codecov || true  # Ignore failures from codecov tool
