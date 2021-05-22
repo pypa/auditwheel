61e89a37e1d048e3f3183c932902e6c355457340
mayeut <mayeut@users.noreply.github.com>
2021-05-22 21:28:09 +0200

    Update travis.sh

--- tests/travis.sh
+++ tests/travis.sh
@@ -2,6 +2,10 @@
 
 set -exuo pipefail
 
+if [ "$(uname -m)" == "ppc64le" ]; then
+    export DOCKER_HOST=unix://${HOME}/.docker/run/docker.sock
+fi
+
 pytest -s --cov auditwheel --cov-branch
 auditwheel lddtree $(python -c 'import sys; print(sys.executable)')
 codecov || true  # Ignore failures from codecov tool
