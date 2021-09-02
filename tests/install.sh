e070c0ed5b5f53301cd27f0e39f16c08908d157c
Matthieu Darbois <mayeut@users.noreply.github.com>
2021-09-02 22:30:48 +0200

    chore: move tests from `tox` to `nox` (#330)

--- tests/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -exuo pipefail
-
-pip install -r test-requirements.txt
-pip install tox codecov
-pip install -e .
-# pull manylinux images that will be used, this helps passing tests which would otherwise timeout.
-python -c $'from tests.integration.test_manylinux import MANYLINUX_IMAGES\nfor image in MANYLINUX_IMAGES.values():\n    print(image)' | xargs -L 1 docker pull
