2b4e825c19a25eef80d2c7e7665ab5cc8296085a
Matthieu Darbois <mayeut@users.noreply.github.com>
2021-09-25 10:12:25 +0200

    chore: add `build` & `test-dist` nox sessions (#336)
    The `build` session does what you'd expect: it builds SDist & wheel using pypa/build.
    The `test-dist` session builds SDist & wheel then runs unit tests on those with all
    supported python versions.
    
    Also modifies the GHA workflow to fail early if pre-commit checks or test-dist (3.9 only) fail.

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,9 @@ from typing import List
 
 import nox
 
+nox.options.sessions = ["lint", "test-dist"]
+
+PYTHON_ALL_VERSIONS = ["3.6", "3.7", "3.8", "3.9"]
 RUNNING_CI = "TRAVIS" in os.environ or "GITHUB_ACTIONS" in os.environ
 
 
@@ -51,7 +54,7 @@ Path(r"{images_file}").write_text(images)
     return images_file.read_text().splitlines()
 
 
-@nox.session(python=["3.6", "3.7", "3.8", "3.9"])
+@nox.session(python=PYTHON_ALL_VERSIONS)
 def tests(session: nox.Session) -> None:
     """
     Run tests.
@@ -73,3 +76,58 @@ def tests(session: nox.Session) -> None:
             session.run("codecov")
         except nox.command.CommandFailed:
             pass  # Ignore failures from codecov tool
+
+
+def _build(session: nox.Session, dist: Path) -> None:
+    session.install("build")
+    tmp_dir = Path(session.create_tmp()) / "build-output"
+    session.run("python", "-m", "build", "--outdir", str(tmp_dir))
+    (wheel_path,) = tmp_dir.glob("*.whl")
+    (sdist_path,) = tmp_dir.glob("*.tar.gz")
+    dist.mkdir(exist_ok=True)
+    wheel_path.rename(dist / wheel_path.name)
+    sdist_path.rename(dist / sdist_path.name)
+
+
+@nox.session(name="test-dist")
+def test_dist(session: nox.Session) -> None:
+    """
+    Builds SDist & Wheels then run unit tests on those.
+    """
+    tmp_dir = Path(session.create_tmp())
+    dist = tmp_dir / "dist"
+    _build(session, dist)
+    python_versions = session.posargs or PYTHON_ALL_VERSIONS
+    for version in python_versions:
+        session.notify(f"_test_sdist-{version}", [str(dist)])
+        session.notify(f"_test_wheel-{version}", [str(dist)])
+
+
+def _test_dist(session: nox.Session, path: str, pattern: str) -> None:
+    (dist_path,) = Path(path).glob(pattern)
+    session.install("-r", "test-requirements.txt", str(dist_path))
+    session.run("pytest", "tests/unit")
+
+
+@nox.session(python=PYTHON_ALL_VERSIONS)
+def _test_sdist(session: nox.Session) -> None:
+    """
+    Do not run explicitly.
+    """
+    _test_dist(session, session.posargs[0], "*.tar.gz")
+
+
+@nox.session(python=PYTHON_ALL_VERSIONS)
+def _test_wheel(session: nox.Session) -> None:
+    """
+    Do not run explicitly.
+    """
+    _test_dist(session, session.posargs[0], "*.whl")
+
+
+@nox.session
+def build(session: nox.Session) -> None:
+    """
+    Make an SDist and a wheel.
+    """
+    _build(session, Path("dist"))
