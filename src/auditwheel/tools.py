1dd0172ca740900b0620ef721863beff3e898c98
mayeut <mayeut@users.noreply.github.com>
2022-01-07 23:17:07 +0100

    fix: files are not compressed with ZIP_DEFLATED

--- src/auditwheel/tools.py
+++ src/auditwheel/tools.py
@@ -70,19 +70,21 @@ def dir2zip(in_dir: str, zip_fname: str, date_time: Optional[datetime] = None) -
         st = os.stat(in_dir)
         date_time = datetime.fromtimestamp(st.st_mtime, tz=timezone.utc)
     date_time_args = date_time.timetuple()[:6]
-    with zipfile.ZipFile(zip_fname, "w", compression=zipfile.ZIP_DEFLATED) as z:
+    compression = zipfile.ZIP_DEFLATED
+    with zipfile.ZipFile(zip_fname, "w", compression=compression) as z:
         for root, dirs, files in os.walk(in_dir):
             for dir in dirs:
                 dname = os.path.join(root, dir)
                 out_dname = os.path.relpath(dname, in_dir) + "/"
-                zinfo = zipfile.ZipInfo(out_dname, date_time=date_time_args)
-                zinfo.external_attr = os.stat(dname).st_mode << 16
-                z.writestr(zinfo, "")
+                zinfo = zipfile.ZipInfo.from_file(dname, out_dname)
+                zinfo.date_time = date_time_args
+                z.writestr(zinfo, b"")
             for file in files:
                 fname = os.path.join(root, file)
                 out_fname = os.path.relpath(fname, in_dir)
-                zinfo = zipfile.ZipInfo(out_fname, date_time=date_time_args)
-                zinfo.external_attr = os.stat(fname).st_mode << 16
+                zinfo = zipfile.ZipInfo.from_file(fname, out_fname)
+                zinfo.date_time = date_time_args
+                zinfo.compress_type = compression
                 with open(fname, "rb") as fp:
                     z.writestr(zinfo, fp.read())
 
