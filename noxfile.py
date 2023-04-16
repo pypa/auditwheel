51c050ee7889e611514778d83b060aa1a2f6ded5
mayeut <mayeut@users.noreply.github.com>
2023-04-16 15:51:09 +0200

    f

--- noxfile.py
+++ noxfile.py
@@ -73,6 +73,7 @@ def tests(session: nox.Session) -> None:
     session.run("pytest", "-s", *posargs)
     if RUNNING_CI:
         session.run("auditwheel", "lddtree", sys.executable)
+        session.run("coverage", "xml", "-ocoverage.xml")
 
 
 def _build(session: nox.Session, dist: Path) -> None:
