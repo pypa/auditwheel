9d40f3a8af071480ff1bfed65c6a96e83b65ce11
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2023-09-25 20:53:38 +0100

    Remove the `addtag` subcommand
    This subcommand has been broken for at least a few releases and doesn't
    provide any useful functionality. Removing it will at least avoid
    confusing users when they try to use it.

--- tests/integration/test_nonplatform_wheel.py
+++ tests/integration/test_nonplatform_wheel.py
@@ -8,7 +8,7 @@ import pytest
 HERE = pathlib.Path(__file__).parent.resolve()
 
 
-@pytest.mark.parametrize("mode", ["repair", "addtag", "show"])
+@pytest.mark.parametrize("mode", ["repair", "show"])
 def test_non_platform_wheel_repair(mode):
     wheel = HERE / "plumbum-1.6.8-py2.py3-none-any.whl"
     proc = subprocess.run(
