17bdac1aee64f675352581e104a0adea939f1576
mayeut <mayeut@users.noreply.github.com>
2025-01-04 13:23:54 +0100

    feat: add musllinux riscv64 support

--- src/auditwheel/policy/__init__.py
+++ src/auditwheel/policy/__init__.py
@@ -286,6 +286,7 @@ def _fixup_musl_libc_soname(libc: Libc, arch: str, whitelist):
             "s390x": "libc.musl-s390x.so.1",
             "ppc64le": "libc.musl-ppc64le.so.1",
             "armv7l": "libc.musl-armv7.so.1",
+            "riscv64": "libc.musl-riscv64.so.1",
         }
     }
     new_whitelist = []
