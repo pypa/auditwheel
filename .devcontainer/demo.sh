92fc470269b5018815d76bf58bf492add0d6691e
Yichen Yan <wenji.yyc@alibaba-inc.com>
2025-02-19 03:29:32 +0000

    update

--- .devcontainer/demo.sh
+++ .devcontainer/demo.sh
@@ -1,6 +1,10 @@
+#! /bin/bash
+
 auditwheel -v repair \
-    --exclude libcuds.so.1 \
+    --exclude libcuda.so.1 \
     --exclude libcusolver.so.11 \
     --exclude libcusparseLt.so.0 \
     --plat=manylinux_2_35_x86_64 \
-    /torch-2.6.0-cp312-cp312-manylinux1_x86_64.whl
+    -w /tmp/wheelhouse \
+    /torch-2.6.0-cp312-cp312-manylinux1_x86_64.whl \
+    2>&1 | ts '[%Y-%m-%d %H:%M:%S]'
