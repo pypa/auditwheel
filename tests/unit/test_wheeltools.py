8a0413ff551aa22547bd2391a75c63f94b521ab2
mayeut <mayeut@users.noreply.github.com>
2025-02-24 07:22:40 +0100

    add unit test for get_wheel_libc

--- tests/unit/test_wheeltools.py
+++ tests/unit/test_wheeltools.py
@@ -6,7 +6,12 @@ import pytest
 
 from auditwheel.architecture import Architecture
 from auditwheel.error import NonPlatformWheel
-from auditwheel.wheeltools import WheelToolsError, get_wheel_architecture
+from auditwheel.libc import Libc
+from auditwheel.wheeltools import (
+    WheelToolsError,
+    get_wheel_architecture,
+    get_wheel_libc,
+)
 
 
 @pytest.mark.parametrize(
@@ -41,3 +46,33 @@ def test_get_wheel_architecture_multiple(filename: str) -> None:
     match = re.escape("multiple architectures are not supported")
     with pytest.raises(WheelToolsError, match=match):
         get_wheel_architecture(filename)
+
+
+@pytest.mark.parametrize(
+    ("filename", "expected"),
+    [
+        ("foo-1.0-py3-none-manylinux1_x86_64.whl", Libc.GLIBC),
+        ("foo-1.0-py3-none-manylinux1_x86_64.manylinux2010_x86_64.whl", Libc.GLIBC),
+        ("foo-1.0-py3-none-musllinux_1_1_x86_64.whl", Libc.MUSL),
+    ],
+)
+def test_get_wheel_libc(filename: str, expected: Libc) -> None:
+    libc = get_wheel_libc(filename)
+    assert libc == expected
+
+
+@pytest.mark.parametrize(
+    "filename", ["foo-1.0-py3-none-any.whl", "foo-1.0-py3-none-something.whl"]
+)
+def test_get_wheel_libc_unknown(filename: str) -> None:
+    with pytest.raises(WheelToolsError, match=re.escape("unknown libc used")):
+        get_wheel_libc(filename)
+
+
+@pytest.mark.parametrize(
+    "filename", ["foo-1.0-py3-none-manylinux1_x86_64.musllinux_1_1_x86_64.whl"]
+)
+def test_get_wheel_libc_multiple(filename: str) -> None:
+    match = re.escape("multiple libc are not supported")
+    with pytest.raises(WheelToolsError, match=match):
+        get_wheel_libc(filename)
