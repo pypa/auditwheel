d978dc110c3baf44f6ebaa86420094e2d34f3356
mayeut <mayeut@users.noreply.github.com>
2025-03-30 16:58:25 +0200

    fix heuristic

--- src/auditwheel/lddtree.py
+++ src/auditwheel/lddtree.py
@@ -527,7 +527,12 @@ def ldd(
             if path.name.endswith(("-arm-linux-musleabihf.so", "-linux-musl.so")):
                 libc = Libc.MUSL
             elif path.name.endswith(("-arm-linux-gnueabihf.so", "-linux-gnu.so")):
-                libc = Libc.GLIBC
+                # before python 3.11, musl was also using gnu
+                soabi = path.stem.split(".")[-1].split("-")
+                valid_python = tuple(f"3{minor}" for minor in range(11, 100))
+                if soabi[0] == "cpython" and soabi[1].startswith(valid_python):
+                    libc = Libc.GLIBC
+
         if ldpaths is None:
             ldpaths = load_ld_paths(libc).copy()
         # Propagate the rpaths used by the main ELF since those will be
