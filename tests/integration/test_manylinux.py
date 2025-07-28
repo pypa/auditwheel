a937698b051c9f27eb150e353441e129b13678c8
Asif Saif Uddin {"Auvi":"অভি"} <auvipy@gmail.com>
2025-07-28 16:04:07 +0600

    Update tests/integration/test_manylinux.py
    Co-authored-by: Matthieu Darbois <mayeut@users.noreply.github.com>

--- tests/integration/test_manylinux.py
+++ tests/integration/test_manylinux.py
@@ -585,8 +585,10 @@ class Anylinux:
         # Package URL prefixes must match for a policy.
         if policy.startswith("musllinux"):
             expected_purl_prefix = "pkg:apk/alpine/"
-        elif policy.startswith(("manylinux_2_17", "manylinux_2_31_")):
-            expected_purl_prefix = ("pkg:rpm/centos/", "pkg:deb/ubuntu/")
+        elif policy.startswith("manylinux_2_17_"):
+            expected_purl_prefix = "pkg:rpm/centos/"
+        elif policy.startswith("manylinux_2_31_"):
+            expected_purl_prefix = "pkg:deb/ubuntu/"
         elif policy.startswith("manylinux_2_34_"):
             expected_purl_prefix = "pkg:rpm/almalinux/"
         else:
