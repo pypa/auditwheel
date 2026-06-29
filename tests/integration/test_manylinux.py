dd77c67ff9825fa954860bc946e2e2401d65528c
mayeut <mayeut@users.noreply.github.com>
2026-06-29 06:37:23 +0200

    test with patchelf==0.19.1.0

--- tests/integration/test_manylinux.py
+++ tests/integration/test_manylinux.py
@@ -506,6 +506,9 @@ class Anylinux:
                     "chmod +x /usr/local/bin/lief-patchelf",
                 ),
             )
+        else:
+            commands.append("pipx uninstall patchelf")
+            commands.append("pipx install -f patchelf==0.19.1.0")
         return commands
 
     @pytest.fixture
